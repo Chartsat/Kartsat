8e9e3b0eed71870526202667abcc0c28b91bcdb8
Chartsat <harrisforall@gmail.com>
2015-11-08 14:02:16 +0200

    The rest of project without register.class

---
+++ app/src/main/java/com/example/charis/kartsat/GetUserCallback.java
@@ -0,0 +1,10 @@
+package com.example.charis.kartsat;
+
+/**
+ * Created by Charis on 7/11/2015.
+ */
+interface GetUserCallback {
+
+    public  abstract void done(User returnedUser);
+
+}
--- app/src/main/java/com/example/charis/kartsat/MainActivity.java
+++ app/src/main/java/com/example/charis/kartsat/MainActivity.java
@@ -2,13 +2,8 @@ package com.example.charis.kartsat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +11,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
     Button bLogout;
     EditText etName, etAge,etUsername;
+    UserLocalStore userLocalStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +25,39 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         bLogout = (Button) findViewById(R.id.bLogout);
 
         bLogout.setOnClickListener(this);
+
+        userLocalStore = new UserLocalStore(this);
     }
 
-        @Override
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (authenticate() == true) {
+            displayUserDetails();
+        }else{
+            startActivity(new Intent(MainActivity.this,login.class));
+        }
+    }
+
+    private  boolean authenticate(){
+        return userLocalStore.getUserLoggedIn();
+    }
+
+    private  void  displayUserDetails(){
+        User user = userLocalStore.getLoggedInUser();
+
+        etUsername.setText(user.username);
+        etName.setText(user.name);
+        etAge.setText(user.age + "");
+    }
+    @Override
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.bLogout:
+                    userLocalStore.clearUserData();
+                    userLocalStore.setUserLoggedIn(false);
+
+
                     startActivity(new Intent(this, login.class));
                     break;
 
